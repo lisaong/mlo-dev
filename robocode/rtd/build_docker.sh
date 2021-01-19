50c59a28e0f52c09f601515a3fddd0cfdf2f004f
Lisa Ong <onglisa@microsoft.com>
2021-01-19 20:25:59 +0800

    for fun

--- robocode/rtd/build_docker.sh
+++ robocode/rtd/build_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --build-arg REPO_PAT=PATHERE -t=lisaong/robocode-rtdserver:1.0 -t=lisaong/robocode-rtdserver:latest --no-cache .
+docker build --build-arg REPO_PAT=PATHERE -t=lisaong/robocode-rtdserver:1.2 -t=lisaong/robocode-rtdserver:latest --no-cache .
