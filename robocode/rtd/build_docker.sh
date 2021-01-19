5ff8438e4ad1e3c5ed10f5aca39d0d55afae94c1
Lisa Ong <onglisa@microsoft.com>
2021-01-19 10:06:49 +0800

    fix cmdline

--- robocode/rtd/build_docker.sh
+++ robocode/rtd/build_docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build --build-arg REPO_PAT=PATPAT -t='lisaong/robocode-rtdserver:1.0' .
+docker build --build-arg REPO_PAT=PATHERE -t=lisaong/robocode-rtdserver:1.0 .
 docker tag lisaong/robocode-rtdserver:1.0 lisaong/robocode-rtdserver:latest
