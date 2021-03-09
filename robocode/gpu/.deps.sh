cefa564d6c2de75adbb227307a18d54fc25bb821
Lisa Ong <onglisa@microsoft.com>
2021-03-09 11:19:35 +0800

    azure gpu scripts

---
+++ robocode/gpu/.deps.sh
@@ -0,0 +1,3 @@
+conan remote add robocode http://robocode-packages.eastus.cloudapp.azure.com:8081/artifactory/api/conan/RoboCode-Packages
+conan user -p RoboCode2020 -r robocode admin
+pip3 install pandas
---
+++ robocode/gpu/run_vulkan.sh
@@ -0,0 +1,1 @@
+sudo docker run --gpus all -it -v /home/azureuser:/code --rm nvidia/vulkan:1.1.121
