ed03f8add5c60b9a87ee05963bd8273ee62b82db
Lisa Ong <onglisa@microsoft.com>
2023-05-16 01:38:59 +0000

    scaffold

--- banded_matmul/run.sh
+++ banded_matmul/run.sh
@@ -6,6 +6,6 @@ rm -rf bmm
 nvcc --use_fast_math --std=c++17 -o bmm bmm.cu
 ./bmm 0
 
-# rm -rf bmm_smem
-# nvcc --use_fast_math --std=c++17 -o bmm_smem bmm_smem.cu
-# ./bmm_smem 0
+rm -rf bmm_smem
+nvcc --use_fast_math --std=c++17 -o bmm_smem bmm_smem.cu
+./bmm_smem 0
