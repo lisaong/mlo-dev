a798fd0559993add12a3eef874ba903395760538
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2023-05-17 16:52:10 +0800

    Non-functional experiment with wmma (#14)
    * templatize helper classes
    
    * experimental wmma based on cuda sample
    
    * fix build errors
    
    * set correct arch
    
    * runs but very slow
    
    * debug
    
    * WIP
    
    ---------
    
    Co-authored-by: Lisa Ong <onglisa@microsoft.com>

---
+++ banded_matmul/bmm_wmma.cu
@@ -0,0 +1,202 @@
+// Banded matrix multiplication using WMMA
+// cf: simple_wmma_tf32gemm from
+// https://github.com/NVIDIA/cuda-samples/blob/master/Samples/3_CUDA_Features/tf32TensorCoreGemm/tf32TensorCoreGemm.cu
+
+#include <cuda_runtime.h>
+#include <mma.h>
+using namespace nvcuda;
+
+#include <cstdint>
+
+// #define DEBUG 1
+#include "constants.h"
+#include "utils.h"
+
+// constexpr int WARP_SIZE = 32;
+// constexpr int WARPS_PER_BLOCK = 8;
+// constexpr int THREADS_PER_BLOCK = WARP_SIZE * WARPS_PER_BLOCK;
+
+constexpr int WMMA_M = 16;
+constexpr int WMMA_N = 16;
+constexpr int WMMA_K = 16;
+
+// constexpr int M_TILES = N / WMMA_M;
+// constexpr int N_TILES = N / WMMA_N;
+// constexpr int  K_TILES = N / WMMA_K;
+
+// TODO: Not yet correct
+__global__ void bandedMatMul_wmma(int n0, int n1, int n2, float *t0,
+                                  const half *t1, const half *t2) {
+
+  // Leading dimensions
+  int lda = n2;
+  int ldb = n2;
+  int ldc = n1;
+
+  // Tile using a 2D grid
+  int warpM = (blockIdx.x * blockDim.x + threadIdx.x) / warpSize;
+  int warpN = (blockIdx.y * blockDim.y + threadIdx.y);
+
+  // Declare the fragments
+  wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major>
+      a_frag;
+  wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major>
+      b_frag;
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> acc_frag;
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, float> c_frag;
+
+  wmma::fill_fragment(acc_frag, 0.0f);
+
+  // Loop over k
+  for (int i = 0; i < n2; i += WMMA_K) {
+    int aCol = i;
+    int aRow = warpM * WMMA_M;
+    int bCol = warpN * N;
+    int bRow = i;
+
+    // Bounds checking
+    if (aRow < n0 && aCol < n2 && bRow < n2 && bCol < n1) {
+      // Load the inputs
+      wmma::load_matrix_sync(a_frag, t1 + aCol + aRow * lda, lda);
+      wmma::load_matrix_sync(b_frag, t2 + (aRow + bRow) + bCol * ldb, ldb);
+
+      // Perform the matrix multiplication
+      wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
+    }
+  }
+
+  // Load in the current value of C and add our result
+  int cCol = warpN * WMMA_N;
+  int cRow = warpM * WMMA_M;
+
+  if (cRow < n0 && cCol < n1) {
+    wmma::load_matrix_sync(c_frag, t0 + cCol + cRow * ldc, ldc,
+                           wmma::mem_row_major);
+
+    for (int i = 0; i < c_frag.num_elements; i++) {
+      c_frag.x[i] += acc_frag.x[i];
+    }
+
+    // Store the output
+    wmma::store_matrix_sync(t0 + cCol + cRow * ldc, c_frag, ldc,
+                            wmma::mem_row_major);
+  }
+}
+
+void run(int deviceId) {
+
+  const int n0 = N; // n0: number of rows in T0 and T1
+  const int n1 = N; // n1: number of columns in T0 and T2
+  const int n2 = N; // n2: inner or shared dimension, i.e.
+                    //     number of columns in T1 and number of rows in T2
+
+  Matrix<float> T0(n0, n1);            // output
+  BandedMatrix<half> T1(n0, kBandDim); // input
+  Matrix<half> T2(T1.columns(), n1);   // input
+
+  CHECK(cudaMallocManaged(&T0.data, T0.size()));
+  CHECK(cudaMallocManaged(&T1.data, T1.size()));
+  CHECK(cudaMallocManaged(&T2.data, T2.size()));
+
+  // Initialize
+  dim3 threadsInit(kBlockDim, kBlockDim, 1);
+  dim3 blocksInit(n0 / threadsInit.x, n1 / threadsInit.y, 1);
+  initWith<<<blocksInit, threadsInit>>>(11.0f, T0.data, T0.rows(),
+                                        T0.columns());
+  initBandedWith<<<blocksInit, threadsInit>>>(22.0f, T1.data, T1.rows(),
+                                              T1.columns(), T1.band());
+  initWith<<<blocksInit, threadsInit>>>(33.0f, T2.data, T2.rows(),
+                                        T2.columns());
+  CHECK(cudaDeviceSynchronize());
+
+  // Verify
+  dim3 gridDim;
+  dim3 blockDim;
+
+  blockDim.x = 128;
+  blockDim.y = 4;
+
+  gridDim.x =
+      (n0 + (WMMA_M * blockDim.x / 32 - 1)) / (WMMA_M * blockDim.x / 32);
+  gridDim.y = (n1 + WMMA_N * blockDim.y - 1) / (WMMA_N * blockDim.y);
+
+  bandedMatMul_wmma<<<gridDim, blockDim>>>(n0, n1, n2, T0.data, T1.data,
+                                           T2.data);
+  CHECK(cudaGetLastError());
+  CHECK(cudaDeviceSynchronize());
+
+  if (checkCorrectness(n0, n1, n2, T0, T1, T2)) {
+
+#if 0
+    // Benchmark
+    cudaEvent_t _start;
+    cudaEvent_t _stop;
+    cudaEventCreate(&_start);
+    cudaEventCreate(&_stop);
+
+    std::cout << "GridDim,BlockDim,FLOPS,GFLOPS" << std::endl;
+
+    // TODO: fix
+    // Try different block sizes
+    for (uint32_t blockDim = WARP_SIZE; blockDim <= kMaxBlockDim;
+         blockDim += WARP_SIZE) {
+
+      dim3 threads(blockDim, blockDim, 1);
+      dim3 blocks(ceildiv(n0, threads.x), ceildiv(n1, threads.y), 1);
+
+      try {
+        double elapsedTimeMilliseconds = 0.0f;
+        uint64_t iterations = 0;
+        float duration = 0.0f;
+
+        // Runs the function until 10 seconds has elapsed
+        cudaEventRecord(_start);
+        while (elapsedTimeMilliseconds < kTimelimit) {
+          bandedMatMul_wmma<<<blocks, threads>>>(n0, n1, n2, T0.data, T1.data,
+                                                 T2.data);
+
+          CHECK(cudaGetLastError());
+          CHECK(cudaDeviceSynchronize());
+
+          cudaEventRecord(_stop);
+          cudaEventSynchronize(_stop);
+          cudaEventElapsedTime(&duration, _start, _stop);
+          elapsedTimeMilliseconds += duration;
+          iterations++;
+        }
+
+        const double flops = iterations * kNumberOfOps /
+                             (elapsedTimeMilliseconds / kMillisecondsInSeconds);
+        std::cout << blocks.x << "," << threads.x << "," << flops << ","
+                  << flops / 1e9 << std::endl;
+      } catch (const std::exception &e) {
+        std::cout << "Skipping Blocksize: " << blockDim << ", " << e.what()
+                  << std::endl;
+        continue;
+      }
+    }
+
+    cudaEventDestroy(_start);
+    cudaEventDestroy(_stop);
+#endif
+  }
+
+  cudaFree(T0.data);
+  cudaFree(T1.data);
+  cudaFree(T2.data);
+}
+
+int main(int argc, const char **argv) {
+  int deviceId;
+
+  if (argc > 1) {
+    deviceId = atoi(argv[1]);
+    CHECK(cudaSetDevice(deviceId));
+  } else {
+    CHECK(cudaGetDevice(&deviceId));
+  }
+  std::cout << "Using device " << deviceId << std::endl;
+
+  run(deviceId);
+  return 0;
+}
--- banded_matmul/utils.h
+++ banded_matmul/utils.h
@@ -2,8 +2,10 @@
 
 #include <cstdint>
 #include <cstdlib>
-#include <cuda_runtime.h>
 #include <iostream>
+
+#include <cuda_runtime.h>
+#include <mma.h>
 
 void CHECK(cudaError_t res) {
   if (cudaSuccess != res) {
@@ -12,6 +14,18 @@ void CHECK(cudaError_t res) {
 }
 
 __global__ void initWith(float num, float *a, int rows, int columns) {
+
+  int i, j;
+  for (i = blockIdx.x * blockDim.x + threadIdx.x; i < rows;
+       i += blockDim.x * gridDim.x) {
+    for (j = blockIdx.y * blockDim.y + threadIdx.y; j < columns;
+         j += blockDim.y * gridDim.y) {
+      a[i * columns + j] = num;
+    }
+  }
+}
+
+__global__ void initWith(half num, half *a, int rows, int columns) {
 
   int i, j;
   for (i = blockIdx.x * blockDim.x + threadIdx.x; i < rows;
@@ -42,7 +56,26 @@ __global__ void initBandedWith(float num, float *a, int rows, int columns,
   }
 }
 
-class Matrix {
+__global__ void initBandedWith(half num, half *a, int rows, int columns,
+                               int band) {
+
+  int i, j;
+  for (i = blockIdx.x * blockDim.x + threadIdx.x; i < rows;
+       i += blockDim.x * gridDim.x) {
+    for (j = blockIdx.y * blockDim.y + threadIdx.y; j < band;
+         j += blockDim.y * gridDim.y) {
+
+      if ((i + j) < columns) {
+        a[i * band + j] = num;
+      } else {
+        // zero out the lower right triangle
+        a[i * band + j] = 0;
+      }
+    }
+  }
+}
+
+template <typename T> class Matrix {
 
 public:
   Matrix(int rows, int columns) : _rows(rows), _columns(columns) {}
@@ -50,9 +83,9 @@ public:
   int rows() const { return _rows; }
   int columns() const { return _columns; }
   uint64_t numElements() const { return _rows * _columns; }
-  uint64_t size() const { return numElements() * sizeof(float); }
+  uint64_t size() const { return numElements() * sizeof(T); }
 
-  void init(float value) {
+  void init(T value) {
     for (uint64_t i = 0; i < numElements(); ++i) {
       data[i] = value;
     }
@@ -61,7 +94,7 @@ public:
   void randomInit(int seed) {
     srand(seed);
     for (uint64_t i = 0; i < numElements(); ++i) {
-      data[i] = static_cast<float>(rand()) / RAND_MAX;
+      data[i] = static_cast<T>(rand()) / RAND_MAX;
     }
   }
 
@@ -89,18 +122,18 @@ public:
     }
   }
 
-  float *data;
+  T *data;
 
 protected:
   int _rows;
   int _columns;
 };
 
-class BandedMatrix : public Matrix {
+template <typename T> class BandedMatrix : public Matrix<T> {
 
 public:
-  BandedMatrix(int rows, int band) : Matrix(rows, band), _band(band) {}
-  int columns() const { return _rows + _band; }
+  BandedMatrix(int rows, int band) : Matrix<T>(rows, band), _band(band) {}
+  int columns() const { return this->_rows + _band; }
   int band() const { return _band; }
 
   // The lower right triangle of a banded matrix is typcially padded with zeros.
@@ -114,13 +147,13 @@ public:
   // [ 0 0 0 0 0 0 0 x ]      [ x 0 0 ]  A[7, 1] = A[7, 2] = 0
 
   void init(float value) {
-    for (int i = 0; i < rows(); ++i) {
+    for (int i = 0; i < this->rows(); ++i) {
       for (int j = 0; j < _band; ++j) {
         if ((i + j) < columns()) {
-          data[i * _band + j] = value;
+          this->data[i * _band + j] = value;
         } else {
           // zero out the lower right triangle
-          data[i * _band + j] = 0;
+          this->data[i * _band + j] = 0;
         }
       }
     }
@@ -148,11 +181,30 @@ void bandedMatMul_CPU(int n0, int n1, int n2, float *t0, const float *t1,
   }
 }
 
-bool checkCorrectness(int n0, int n1, int n2, const Matrix &T0,
-                      const BandedMatrix &T1, const Matrix &T2) {
-  Matrix T0_CPU(T0.rows(), T0.columns());
+void bandedMatMul_CPU(int n0, int n1, int n2, float *t0, const half *t1,
+                      const half *t2) {
+  /*
+    for i in range(n0):
+        for j in range(n1):
+            for k in range(n2):
+                t0[i, j] += t1[i, k] * t2[i + k, j]
+  */
+  int i, j, k;
+  for (i = 0; i < n0; ++i) {
+    for (j = 0; j < n1; ++j) {
+      for (k = 0; k < n2 && (i + k) < n0; ++k) {
+        t0[i * n1 + j] += (float)t1[i * n2 + k] * (float)t2[(i + k) * n1 + j];
+      }
+    }
+  }
+}
 
-  T0_CPU.data = reinterpret_cast<float *>(malloc(T0_CPU.size()));
+template <typename TIn, typename TOut>
+bool checkCorrectness(int n0, int n1, int n2, const Matrix<TOut> &T0,
+                      const BandedMatrix<TIn> &T1, const Matrix<TIn> &T2) {
+  Matrix<TOut> T0_CPU(T0.rows(), T0.columns());
+
+  T0_CPU.data = reinterpret_cast<TOut *>(malloc(T0_CPU.size()));
   T0_CPU.init(11.0f);
 
   bandedMatMul_CPU(n0, n1, n2, T0_CPU.data, T1.data, T2.data);
