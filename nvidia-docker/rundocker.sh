12759c06d4a569472264f0fd8d14f6219e2f8f84
Lisa Ong <onglisa@microsoft.com>
2023-05-23 02:10:05 -0700

    update docker script

--- nvidia-docker/rundocker.sh
+++ nvidia-docker/rundocker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker run -it --cap-add=SYS_ADMIN --gpus all \
-    -v /home/onglisa/accera.triton:/accera.triton \
-    -v /home/onglisa/mlo-dev:/mlo-dev \
+    -v $HOME/accera.triton:/accera.triton \
+    -v $HOME/mlo-dev:/mlo-dev \
     -u $(id -u):$(id -g) \
     cuda_12.1.1
