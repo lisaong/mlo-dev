ef5099cc97dba03ab9d8f4b6acca3fd963ca5f74
Lisa Ong <onglisa@microsoft.com>
2023-06-22 02:23:46 -0700

    add marker to plots

--- hip/plot.py
+++ hip/plot.py
@@ -7,7 +7,7 @@ import pandas as pd
 def plot(csv, x, y, output):
     "Usage: plot result.csv block_size elapsed_msec result_plot.png"
     df = pd.read_csv(csv)
-    df.plot(x=x, y=y);
+    df.plot(x=x, y=y, marker='o');
     plt.grid()
     plt.savefig(output)
 
