f656123cf0c64d6db5b3ba88af595232f51fb6ad
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2023-06-22 14:45:27 +0800

    Add elapsed timer and print grid and block sizes (#31)
    * add timer region
    
    * print grid and block sizes
    
    ---------
    
    Co-authored-by: Lisa Ong <onglisa@microsoft.com>

---
+++ hip/inc/timed_region.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+
+#include <hip/hip_runtime.h>
+
+// cf. https://github.com/ROCm-Developer-Tools/hipamd/blob/develop/src/hip_event.cpp
+
+#ifndef HIP_ASSERT
+#define HIP_ASSERT(x) (assert((x) == hipSuccess))
+#endif
+
+struct TimedRegion
+{
+    hipEvent_t start;
+    hipEvent_t end;
+
+    TimedRegion()
+    {
+        HIP_ASSERT(hipEventCreate(&start));
+        HIP_ASSERT(hipEventCreate(&end));
+        HIP_ASSERT(hipEventRecord(start, 0));
+    }
+
+    ~TimedRegion()
+    {
+        HIP_ASSERT(hipEventRecord(end, 0));
+        HIP_ASSERT(hipEventSynchronize(end)); // wait for event to complete
+
+        float millisecs = 0;
+        HIP_ASSERT(hipEventElapsedTime(&millisecs, start, end));
+        std::cout << ">> Elapsed time (ms): " << millisecs << std::endl;
+
+        HIP_ASSERT(hipEventDestroy(start));
+        HIP_ASSERT(hipEventDestroy(end));
+    }
+};
